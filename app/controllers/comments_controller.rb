6ad16699a04a1055a604afb93dd446e3b3a1c7d6
Lcid Lumenario <lumenariolcid@gmail.com>
2025-06-26 17:09:50 +0800

    added complete CRUD for comments model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_article
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article), status: :see_other
   end
@@ -15,5 +26,13 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
+    end
+
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
+    def set_comment
+      @comment = @article.comments.find(params[:id])
     end
 end
